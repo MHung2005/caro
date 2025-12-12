ba1a99ea34a1a976e40e73c4799c9674e1e49aa1
dungroland <DUNG.NH235052@sis.hust.edu.vn>
2025-12-12 09:07:35 +0700

    Update ai_easy.py

--- ai/ai_easy.py
+++ ai/ai_easy.py
@@ -3,11 +3,11 @@ import random
 class AIEasy:
     def __init__(self, symbol):
         self.symbol = symbol
-    
+        self.node_count = 0
     def choose_move(self, board_status, num_board = 10):
         empty_cells = [
             (r, c) for r in range(num_board)
             for c in range(num_board)
             if board_status[r][c] == 0
         ]
-        return random.choice(empty_cells)
+        return random.choice(empty_cells), self.node_count
